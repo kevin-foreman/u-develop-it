47b796214b1c932ced5bf25329490716528f91c2
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-18 15:44:14 -0500

    Added CRUD queries, and commented them out

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const mysql = require('mysql2');
+
 const express = require('express');
+const { brotliDecompress } = require('zlib');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,11 +13,69 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
+// Connect to database
 
+const db = mysql.createConnection(
+    {
+        host: `localhost`,
+
+        // Your mySQL username
+
+        user: `root`,
+
+        // Your mySQL password
+
+        password: `ISEEkwel!@#123`,
+
+        database: `election`
+    },
+
+    console.log(`Connected to the elecetion database.`)
+);
+
+// db.query('SELECT * FROM candidates', (err, rows) => {
+//     console.log(rows);
+// });
+
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+
+//     VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Firbank'. 1];
+
+// db.query(sql, params, (err, result) => {
+
+//     if (err) {
+
+//         console.log(err);
+//     }
+
+//     console.log(result);
+// });
+
+
+// DELETE query
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+
+//     console.log(row);
+// });
 
 
 // Default response for any other request (Not Found)
-
 app.use((req, res) => {
 
     res.status(404).end();
