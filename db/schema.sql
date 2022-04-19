483fe8bdf0b0dd98123c3c25854491d91e535cf5
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-19 16:33:58 -0500

    Added votes table, and cascading delete functionality

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,11 @@
+DROP TABLE IF EXISTS votes;
+
 DROP TABLE IF EXISTS candidates;
 
 DROP TABLE IF EXISTS parties;
 
 DROP TABLE IF EXISTS voters;
+
 
 CREATE TABLE parties (
 
@@ -27,8 +30,8 @@ CREATE TABLE candidates (
     industry_connected BOOLEAN NOT NULL,
 
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-
 );
+
 
 CREATE TABLE voters (
 
@@ -41,5 +44,22 @@ CREATE TABLE voters (
     email VARCHAR(50) NOT NULL,
 
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    
+);
+
+
+CREATE TABLE votes (
+
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+
+    voter_id INTEGER NOT NULL,
+
+    candidate_id INTEGER NOT NULL,
+
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT uc_voter UNIQUE (voter_id),
+
+    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
